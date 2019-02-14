45b5758ef7e1c774c6f9193590fdbad1b9c3a791
Elena Bernardi <elenabernardi15@gmail.com>
2019-02-14 17:09:31 +0100

    Primo commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
+/*public class Application {
+      public static void main(String[] args){
+          System.out.println("Hello World");
+      }
+}*/
